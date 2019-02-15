26949862a6fb2b4b06fe1fdacf50be190cab46f0
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2019-02-15 15:20:28 +0000

    registration.php improved simple registration.html

--- registration.php
+++ registration.php
@@ -1,3 +1,5 @@
+
+
 <?php
 // define variables and set to empty values
 
@@ -14,20 +16,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT email FROM users";
+$sql = "SELECT Email FROM Users";
 $email_check = $conn->query($sql);
-
-$sql2 = "SELECT name, email FROM team";
-$teamResult = $conn -> query($sql2);
 
 
 
 $name = $lname = $email = "";
-$nameErr = $lnameErr = $emailErr = "";
+$nameErr = $emailErr = $passErr = "";
 $countOfSuccesfulFields = 0;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {/*copied from www.w3school.com */
   session_start();
+  
+  // Name
   if (empty($_POST["name"])) {
     $nameErr = "First name is required";
   } else {
@@ -36,72 +37,69 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {/*copied from www.w3school.com */
     $countOfSuccesfulFields ++;
   }
 
+
+  // PASS
   if (empty($_POST["pass"])) {
     $passErr = "Empty field";
+  } else{
+    $countOfSuccesfulFields ++;
   }
-  else if($_POST["pass"] != $_POST["pass2"])
+
+
+
+  // PASS2
+  if (empty($_POST["pass2"])) {
+    $passErr = "Empty field";
+  }
+  else if($_POST["pass2"] != $_POST["pass"])
   {
     $passErr = "Do not match";
   } 
   else {
-    $name = test_input($_POST["name"]);
-    $_SESSION["name"] = $name;
+    $pass = test_input($_POST["pass"]);
     $countOfSuccesfulFields ++;
   }
 
-  $isTaken = false;
-  while($row = $email_check->fetch_assoc()) 
-    if($row["email"] == $email)
-      $isTaken = true;
   
+  
+  
+
+
+  // EMAIL
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
   } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {/*copied from www.w3school.com */
     $emailErr = "Invalid email format";
   }
-  else if(isTaken)
-  {
-    $emailErr = "Email has already been taken";
-  }
   else{
     $email = test_input($_POST["email"]);
-    $_SESSION["email"] = $email;
-    $countOfSuccesfulFields ++;
+    $isTaken = false;
+    while($row = $email_check->fetch_assoc()) 
+      if($row["Email"] == $email)
+        $isTaken = true;
+    if(isTaken)
+    {
+      $emailErr = "Email has already been taken";
+    }
+    else
+    {
+      $_SESSION["email"] = $email;
+      $countOfSuccesfulFields ++;
+    }
   }
   
-  if($countOfSuccesfulFields == 3)
+  if($countOfSuccesfulFields == 4)
   {
         
-    $insertQuery = "INSERT INTO users (Name, Email, Pass)
+    $insertQuery = "INSERT INTO Users (Name, Email, Pass)
                 VALUES ('{$name}', '{$email}', '{$pass}')";
 
     if($conn->query($insertQuery) === TRUE)
-      header('Location: greeting.php');
+      echo "New record created successfully";
+      //header('Location: greeting.php');
     else
-      ?> <html>something went wrong </html>><?php 
+      echo "Error";
 
-    /*
-        $_SESSION["fromPeople"] = 0;
-        while($row = $result->fetch_assoc()) 
-            if($row["name"] == $name && $row["lname"] = $lname && $row["email"] == $email)
-                $_SESSION["fromPeople"] = 1;
-        
-        
-        
-        $_SESSION["fromTeam"] = 0;            
-        while($row = $teamResult->fetch_assoc()) 
-            if($row["name"] == $name && $row["lname"] = $lname && $row["email"] == $email)
-                $_SESSION["fromTeam"] = 1;   
-                
-        if($_SESSION["fromPeople"] == 0 && $_SESSION["fromTeam"] == 0)
-        {
-            
-            if($conn->query($insertQuery) === TRUE)
-                $_SESSION["insertStatus"] = 1;
-            else
-                $_SESSION["insertStatus"] = 0; 
-         }           
-      */
         
   }//if
 }//if
@@ -114,3 +112,36 @@ function test_input($data) {/*copied from www.w3school.com */
 }
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+Registration form.
+<form method="post" action="/lost/Lost_and_Found/registration.php">
+  <fieldset>
+    <legend>Personal information:</legend>
+    First name:<br>
+    <input type="text" name="name" value="<?php echo $name;?>" required>
+    <span class="error">* <?php echo $nameErr;?> </span>
+    <br>
+    Email:<br>
+    <input type="email" name="email" value="<?php echo $email;?>" required>
+    <span class="error">* <?php echo $emailErr;?> </span>
+    <br>
+    Password:<br>
+    <input type="password" name="pass" required>
+    <span class="error">* <?php echo $passErr;?> </span>
+    <br>
+    Repeat Password:<br>
+    <input type="password" name="pass2" required>
+    <span class="error">* <?php echo $pass2Err;?> ></span>
+    <br>
+    <br>
+    <input type="submit" value="Submit">
+  </fieldset>
+</form>
+
+</body>
+</html>
