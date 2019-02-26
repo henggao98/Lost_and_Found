6313f4b9cb04a24aa6281f66bca93bd266cba363
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2019-02-26 11:33:08 +0000

    items page pagination

--- showItems.php
+++ showItems.php
@@ -1,86 +1,92 @@
 <?php    
-  while($row = $result->fetch_assoc()): 
-    
-    if(empty($category) && empty($location) && empty($searched)): ?>
-        <div class="card"><h2>
-        <?php echo $row["ItemName"]; ?>
-        </h2><p class="outset"><h4>
-        <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-        </h4></p><p>
-        <?php echo $row["Descript"]; ?>  
-        </p></div>
-    <?php 
-    elseif(empty($location) && !empty($category) && empty($searched) && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false): ?>
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
-    <?php
-    elseif(empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false ): ?>
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
-    <?php
-    elseif(!empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false): ?>
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
-  <?php
-    elseif(empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false): ?>
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
-    
-    <?php
-    elseif(!empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false): ?> 
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
-    
-    <?php
-    elseif(empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false): ?> 
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
 
-    <?php
-    elseif(!empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false): ?> 
+$counter = 0;
+// COUNTING THE ITEMS.
+while($row = $result->fetch_assoc()){
+        if(empty($category) && empty($location) && empty($searched))
+            $counter++;
+        elseif(empty($location) && !empty($category) && empty($searched) && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false)
+            $counter++;
+        elseif(empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false )
+            $counter++;
+        elseif(!empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false)
+            $counter++;
+        elseif(empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false)
+            $counter++;
+        elseif(!empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false) 
+            $counter++;
+        elseif(empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false) 
+            $counter++;
+        elseif(!empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false)
+            $counter++;
+}
 
-    <div class="card"><h2>
-    <?php echo $row["ItemName"]; ?>
-    </h2><p class="outset"><h4>
-    <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-    </h4></p><p>
-    <?php echo $row["Descript"]; ?>  
-    </p></div>
+if($totalItems != $counter){
+    $totalItems = $counter;
+    $totalPages = ceil($totalItems / $itemsPerPage);
+}
 
+$startItemIndex = ($currentPage-1) * ($itemsPerPage);
+$counter = 0;
+$index = -$startItemIndex;
+$endItemIndex = $currentPage * $itemsPerPage;
 
+$result1 = $conn->query($sql);
 
-  <?php
-    endif; 
-    endwhile;
-    
+// SHOWING THE ITEMS.
+while($row = $result1->fetch_assoc()){
+    if($counter < $totalItems && $counter < $itemsPerPage && $index >= 0)
+    {
+        if(empty($category) && empty($location) && empty($searched))
+        { 
+            include 'item.php';
+            $counter++;
+            $indexCounterState = 0;
+        }
+        elseif(empty($location) && !empty($category) && empty($searched) && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false){
+            include 'item.php'; 
+            $counter++;
+            $indexCounterState = 1;
+        }
+
+        elseif(empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false ){
+            include 'item.php';
+            $counter++;
+            $indexCounterState = 1;
+        }
+
+        
+        elseif(!empty($category) && !empty($location) && empty($searched) && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false){
+            include 'item.php';
+            $counter++;
+            $indexCounterState = 1;
+        }
+      
+        elseif(empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false){ 
+            include 'item.php'; 
+            $counter++;
+            $indexCounterState = 1;
+        }
+
+        elseif(!empty($category) && empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false){ 
+            include 'item.php'; 
+            $counter++;
+            $indexCounterState = 1;
+        }
+
+        elseif(empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false){ 
+            include 'item.php';
+            $counter++; 
+            $indexCounterState = 1;
+        }
+
+        elseif(!empty($category) && !empty($location) && !empty($searched) && strpos(strtolower($row["Descript"]), strtolower($searched)) !== false && strpos(strtolower($row["Location"]), strtolower($location)) !== false && strpos(strtolower($row["ItemName"]), strtolower($category)) !== false){
+            include 'item.php';
+            $counter++;
+            $indexCounterState = 1;
+        } 
+    }
+    $index++;
+}
+
 ?>
