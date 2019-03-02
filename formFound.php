ce21a2661bb1f3dd1576e621e25da50c2533ee3a
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2019-03-02 10:50:45 +0000

    testing Sean found.php

---
+++ formFound.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+  <form name = "formFound" action="found.php" method="post">
+    <input type="text" name="itemName">
+    <input type="text" name="description">
+    <input type="text" name="location">
+  </form>
+
+</body>
+</html>
