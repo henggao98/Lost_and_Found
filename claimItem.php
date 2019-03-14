b5a472f3aeb21939a551beb0c490a782f2190809
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2019-03-14 01:57:00 +0000

    claim button done which sends an email

--- claimItem.php
+++ claimItem.php
@@ -10,11 +10,38 @@ if(isset($_GET["itemID"]) && isset($_GET["mislayerID"]))
   $itemResult = $conn->query($itemSQL);
   $itemRow = $itemResult->fetch_assoc();
 
+  $finderID = $itemRow["FinderID"];
 
   $mislayerSQL = "SELECT * FROM Users WHERE ID='$mislayerID'";
   $mislayerResult = $conn->query($mislayerSQL);
   $mislayerRow = $mislayerResult->fetch_assoc();
 
+  $finderSQL = "SELECT * FROM Users WHERE ID='$finderID'";
+  $finderResult = $conn->query($finderSQL);
+  $finderRow = $finderResult->fetch_assoc();
+
+  $matchedSQL = "SELECT * FROM Matched WHERE FinderID='$finderID' AND MislayerID='$mislayerID' AND  ItemID='$itemID' ";
+  $matchedResult = $conn->query($matchedSQL);
+  $isInDB = false;
+  if($matchedResult->num_rows == 0)
+  {
+    $insertSQL = "INSERT INTO Matched (FinderID, MislayerID, ItemID, Status) VALUES ('$finderID', '$mislayerID', '$itemID', '0') ";
+    if($conn->query($insertSQL) === TRUE)
+    {
+      echo "New row added";
+      $msg = "The " . $itemRow["ItemName"] . " lost on " . $itemRow["Date"] . " around " . $itemRow["Location"] . " received a match by " . $mislayerRow["Name"] . ". In order to come in contact with the mislayer please email " . $mislayerRow["Email"] . ".";
+      mail($finderRow["Email"], "Lost & Found: Matched case", $msg);
+      //echo $msg;
+    }
+    else
+      echo "Problem with inserting";
+
+  }
+  else
+  {
+    echo "Row already added"; 
+  }
+  header("Location: items.php");
 }
 else
   header("Location: items.php");
