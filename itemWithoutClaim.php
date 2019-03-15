ea625cb1831033db3b87934ca4a458ca5a06dbcb
Jack Westwood <jack.westwood@student.manchester.ac.uk>
2019-03-15 13:35:10 +0000

    Small fix on accounts page

--- itemWithoutClaim.php
+++ itemWithoutClaim.php
@@ -2,7 +2,7 @@
 <?php echo $row["ItemName"]; ?>
 </h2><p class="outset"><h4>
 <?php echo $row["Location"] . ', ' . $row["Date"]; ?>
-</h4></p><p>#
+</h4></p><p>
 <?php echo $row["Descript"]; ?>
 </p>
 </div>
