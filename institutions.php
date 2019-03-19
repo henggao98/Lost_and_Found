67eb41fd5e5ef4f33ba7a6062525439fe99e5f51
Ana <ana-maria.cirtog@student.manchester.ac.uk>
2019-03-19 16:40:33 +0000

    about button added to nav bar

--- institutions.php
+++ institutions.php
@@ -99,6 +99,7 @@ function test_input($data) {
 <div class="topnav">
   <a href="index.php" style="float:right"><i class="fa fa-fw fa-home"></i>Home</a>
   <a href="account.php" style="float:right"><i class="fa fa-fw fa-user"></i>Account</a>
+  <a href="info.html" style="float:right"><i class="fa fa-fw fa-info-circle"></i>About</a>
   <a href="items.php" style="float:right"><i class="fa fa-fw fa-globe"></i>Search Items</a>
   <form name="searchForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <input type="text" name="search" placeholder="Search.." value="<?php echo($_SESSION['search']); ?>">
