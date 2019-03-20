09bf30a94838e559d580298b1baf9f07af599721
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2019-03-20 17:04:54 +0000

    Initial commit

--- showMatchedItemsUserLost.php
+++ showMatchedItemsUserLost.php
@@ -10,16 +10,33 @@
 argument>
 
 <?php
+$itemUSerID = $row['ID']; // SORRY FOR THIS VARIABLE
+$USerID = $_SESSION["name"];
 if($matchedRow["Status"] == 1)
 {
 ?>
-<p><button class="button" onclick="location.href='itemRecieved.php?itemID=<?php echo($row["ID"]) ?> &  matchedID = <?php echo($matchedRow["ID"])?>'">Recieved from Finder</button></p>
+
+<button class="button" onclick="document.getElementById('<?php echo($itemUSerID) ?>').classList.toggle('show');">Recieved from Finder</button>
+
+<div class="formPopup" id="<?php echo($itemUSerID) ?>">
+  <form action="leaveComment.php?CommenterID=<?php echo(arg1) ?>&CommentedID=<?php echo(arg1) ?>" class="formContainer" method="POST">
+
+    <textarea name="textArea" required></textarea><br>
+    
+    <button class="button" onclick="location.href='itemRecieved.php?itemID=<?php echo($row["ID"]) ?> &  matchedID = <?php echo($matchedRow["ID"])?>'" >Don't Leave Comment</button>
+
+    <input type="submit" name="submitButton" class="button">
+
+  </form>
+</div>
+
+
 <?php
 }
 else
 {
 ?>
-<p><button class="button" disabled>Awaiting confirmation from finder</button></p>
+<p><button class="buttonInactive" disabled>Awaiting confirmation from finder</button></p>
 <?php
 }
 ?>
