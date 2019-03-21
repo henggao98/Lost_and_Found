73365116bc62220a6306234a6825c131f45cd54d
z98445xz <xinyu.zhang-5@student.manchester.ac.uk>
2019-03-21 21:43:56 +0000

    Home page facebook button fixed, added more info and footer

--- index.php
+++ index.php
@@ -62,10 +62,9 @@ else
     
     <span class="error"> <?php echo $Error;?> </span>
 
-    <a href="fb-login.php"><button class="facebook">Log in with Facebook!</button></a>
-	
 	<button type="submit" class="popupLogin">Login</button>
   </form>
+  <a href="fb-login.php"><button class="facebook">Log in with Facebook!</button></a>
 </div>
 
 <img src="homePageLogo.png" class="logo">
@@ -90,7 +89,7 @@ else
 		<div class="card">
 			<h2>How to look for a lost item?</h2>	
 			<div class="clearfix"> <i class="fa fa-search" style=" font-size:120px; float:right; padding:10px"></i> Are you looking for an item you misplaced in Manchester? Welcome to <b>Lost&Found</b> where you don't even have to create 
-			an account to look for your belongings! If an item you are looking for has been posted, it is available to view on this <a href="items.html" style="color:#FFBB33">page</a>. 
+			an account to look for your belongings! If an item you are looking for has been posted, it is available to view on this <a href="items.html"  class="links">page</a>. 
 			In order to look for an object you might use the search engine provided or choose from the options enlisted on the side.
 		</div>
 	</div>
@@ -99,7 +98,10 @@ else
 		<div class="card">
 			<h2>How to exchange the item?</h2>
 			<div class="clearfix">
-			<i class="fas fa-coffee" style="font-size:100px; float:right; padding:10px"></i> In order to exchange the item the owner of the item must claim it from the user that uploaded the given item to the <b>Lost&Found</b> website.
+			<i class="fas fa-coffee" style="font-size:100px; float:right; padding:10px"></i>
+			In order to exchange the item the owner of the item must claim it from the user that uploaded the given item to the <b>Lost&Found</b> website. After claiming the item both the claimer and the finder must exchange information about the item making both of the sure its their property, for example exchange information about specific damage of the item or any other specifics coming to mind, may vary with different items. <b> UNDER NO CIRCUMSTANCES SHOULD YOU EXCHANGE PASSWORDS OR PIN CODES! </b> If the finder wants to make sure the claimer is the owner of valuable property like laptop or smartphone, he or she can ask them during the meeting to unlock the device, but as a claimer/owner NEVER PROVIDE THEM WITH THE PASSWORD BEFOREHAND.
+            <br><br>
+            In terms of meetings we highly advise everyone to meet in public spaces such as cafes, libraries, etc. We do so in order to prevent any unpleasant meeting between our users. In case you have a bad experience with a user <b>(i.e. phishing, unpleasant exchange of information or anything else you find unusual)</b> each of the registered users has a comment section, where you can leave a note about your experience or contact us via lost.and.found.um@gmail.com.
 			</div>
 		</div>
 	</div>
@@ -111,31 +113,35 @@ else
 			perfect website for the job! Our website enables you to upload an item in the most convenient ways, you don't even have to 
 			create an account, provide us just with your email and we will take care of the rest!</p>
 			<p>Registering with our website in order to exchange the found item comprises of just 3 simple steps:</p>
-				<h3><a href="registration.html" style="color:#FFBB33">Register</a></h3>
+				<h3>Register</h3>
 					<div class="clearfix">
 					<i class="far fa-address-card" style="font-size:100px; float:left; padding:10px"></i> 
-					As a <b>finder</b> you need to provide us with some details. Our registration process is simple and takes no more than 2 minutes.
-					However, if you don't feel like creating the account on yet another website ours offers a <a href="registration.html" style="color:#FFBB33">guest registration</a>, where you provide us only with
+					As a <b>finder</b> you need to provide us with some details. Our <a href="registration.html" class="links">registration</a> process is simple and takes no more than 2 minutes.
+					However, if you don't feel like creating the account on yet another website ours offers a <a href="registration.html" class="links">guest registration</a>, where you provide us only with
 					your first name and an email enabling the owner of the found property to contact with you.
 					<br>
 					</div>
 				<h3>Complete the item form</h3>
 					<div class="clearfix">
-					<i class="fa fa-upload" style="font-size:120px; float:left; padding:10px"></i>
-					As a <b>finder</b> you need to provide us with some details. Our registration process is simple and takes no more than 2 minutes.
-					However, if you don't feel like creating the account on yet another website ours offers a guest registration, where you provide us only with
-					your first name and an email enabling the owner of the found property to contact with you.<br>
+					<i class="fa fa-upload" style="font-size:120px; float:left; padding:0 10px 0 10px"></i>
+					<br>
+					In order for the item to appear on our website you need to fill out our <b>found item form</b>.You will need to provide us with details about location, where the item was found, type of the object, as well as a short description of it. The <b>important note</b> about the description you provide is that it contains:
+					<ul>
+                    <li>Date you found the item</li>
+                    <li>Description of the place you found it at (i.e. if chosen “public transport” add “found on bus 147”)</li>
+                    <li>Vague description of it (i.e. if the item you found has a specific feature like a specific colour of the etui, or specific scratch or damage <b>DO NOT</b> contain that information in the description.)</li>
+					</ul>
+                    We ask you to write a very vague description allowing you to ask the claimer a more detailed question. We do so in order to make sure the item is returned to its true owner, which can only be achieved if the owner knows the specifics of the item you found.
 					</div>
 				<h3>Meet</h3>
 					<div class="clearfix">
 					<i class="fa fa-handshake-o" style="font-size:87px; float:left; padding:10px"></i>
-					As a <b>finder</b> you need to provide us with some details. Our registration process is simple and takes no more than 2 minutes.
-					However, if you don't feel like creating the account on yet another website ours offers a guest registration, where you provide us only with
-					your first name and an email enabling the owner of the found property to contact with you.
+					Provided that you exchanged information with the claimer and decide that the details they gave you show its their property you must arrange a meeting in order to give the claimed item back to its owner.
 					</div>
 		</div>
 	</div>
   </div>
+  <div class="footer" style="color:#FFBB33"><center><a href="privacy.html" class="links"> Privacy Policy </a> --- <a href="terms.html" class="links"> Terms of use</a></center></div>
 </div>
 
 <script>
