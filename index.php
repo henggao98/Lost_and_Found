7188e3fd0b875e8628eb82df2fead22797e1a0a5
z98445xz <xinyu.zhang-5@student.manchester.ac.uk>
2019-03-22 12:02:34 +0000

    Icons added to home page info part

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 
 <head>
   <link rel="stylesheet" href="homePage2.css">
+  <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
   <title>Lost and Found Home Page</title>
 </head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 <body background="homePageBackground.jpg" class="background">
